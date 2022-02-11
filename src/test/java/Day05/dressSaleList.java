0338744993db5de3ee01c6472c41bc65a180d2af
ParinyaMan <maneesuk.gab@gmail.com>
2022-02-11 13:21:54 -0500

    dressListTask

---
+++ src/test/java/Day05/dressSaleList.java
@@ -0,0 +1,91 @@
+package Day05;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class dressSaleList {
+//			1. go to http://automationpractice.com/index.php
+//		    2. click on DRESSES button
+//		    3. add items on sale to cart and checkout
+//		    4. continue to checkout until you see the 'authentication' page and verify it
+	
+	WebDriver driver;
+	
+
+	@Before
+	public void beforeMethod() {
+
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get("http://automationpractice.com/index.php");
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+	}
+	
+	@Test
+	public void test1() {
+		
+
+		//click dress button
+		driver.findElement(By.xpath("//*[@id=\"block_top_menu\"]/ul/li[2]/a")).click();
+		//list of dress
+		List<WebElement> items = driver.findElements(By.xpath("//*[@id=\"center_column\"]/ul/li"));
+		//access to the item
+//		for (WebElement webElement : items) {
+//			System.out.println(webElement.getText());
+//			
+//		}
+		for (int i = 1; i <= items.size(); i++) {
+			//print product detail
+			WebElement item = driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li["+i+"]"));
+			System.out.println( i + ". "+item.getText());
+			
+			// checking on sale product
+            if(!driver.findElements(By.xpath("//*[@id=\"center_column\"]/ul/li[" + i + "]/div/div[1]/div/div[2]/span[3]")).isEmpty()) {
+                
+                // click on product name
+                driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li["+ i +"]/div/div[2]/h5/a")).click();
+                System.out.println("ON SALE" );
+                // add to cart
+                driver.findElement(By.xpath("//*[@id=\"add_to_cart\"]/button")).click();
+                System.out.println("Added to cart");
+                // click "continue shopping"
+                driver.findElement(By.xpath("//*[@id=\"layer_cart\"]/div[1]")).click();
+                driver.findElement(By.xpath("//*[@id=\"layer_cart\"]/div[1]/div[2]/div[4]/span")).click();
+                // back to product page
+                driver.navigate().back();
+
+            }else {
+                System.out.println("NOT ON SALE ");
+            }
+		}
+            
+            // go to Cart
+            driver.findElement(By.xpath("//*[@id=\"header\"]/div[3]/div/div/div[3]/div/a")).click();
+            // proceed to check out
+            driver.findElement(By.xpath("//*[@id=\"center_column\"]/p[2]/a[1]/span")).click();
+            
+            // verify "Authentication" page
+            String actualTitle = driver.getTitle();
+            String expectedTitle = "Login - My Store";
+            
+            if(expectedTitle.equalsIgnoreCase(actualTitle)) {
+                System.out.println("Passed");
+            }else {
+                System.out.println("Failed");
+                System.out.println("Expected to see > " + expectedTitle);
+                System.out.println("The Driver got  > " + actualTitle);
+            }
+		
+		
+		
+	}
+}	
+
