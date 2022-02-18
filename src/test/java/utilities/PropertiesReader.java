271d00caf3c71cd0b011c43d32ca97689641aac5
ParinyaMan <maneesuk.gab@gmail.com>
2022-02-17 22:12:51 -0500

    update dataFolder

---
+++ src/test/java/utilities/PropertiesReader.java
@@ -0,0 +1,37 @@
+package utilities;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class PropertiesReader {
+
+	private static Properties configFile;
+
+	static {
+
+		try {
+			// giving a file path
+			String filePath = "src/test/resources/dataFolder/config.properties";
+			// open a path to read file
+			FileInputStream input = new FileInputStream(filePath);
+			// init object
+			configFile = new Properties();
+			// load all files and put files to configFile
+			configFile.load(input);
+
+			input.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	//method to call the file 
+	public static String getProperty(String keyName) {
+		return configFile.getProperty(keyName);
+	}
+	
+	
+	
+	
+}
